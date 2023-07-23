d8193a35eeddfbb5d28f3521e1c99893a676da61
eelishap <eelishapathak@bajratechnologies.com>
2023-07-23 16:25:15 +0545

    feat: add check in and check out functionality

--- src/app/shared/attendance/attendance.component.ts
+++ src/app/shared/attendance/attendance.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-attendance',
@@ -8,19 +9,46 @@ import { Router } from '@angular/router';
 })
 export class AttendanceComponent implements OnInit {
 
+  private userId: number;
+
   constructor(
     private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.userId = Number(localStorage.getItem('userId'));
   }
 
   public checkIn(): void {
-
+    if (this.userId) {
+      const now = new Date();
+      if (now.getHours() < 13 || (now.getHours() === 13 && now.getMinutes() <= 30)) {
+        console.log('Checked In:', now);
+        localStorage.setItem('checkIn', now.toString());
+      } else {
+        alert('You can only check in before 1:30 PM.')
+      }
+    }
   }
 
   public checkOut(): void {
-
+    if (this.userId) {
+      const now = new Date();
+      if (localStorage.getItem('checkIn')) {
+        const data = {
+          checkIn: localStorage.getItem('checkIn'),
+          checkOut: now.toString()
+        }
+        this.authService.logAttendance(this.userId, data).subscribe(res => {
+          if(res) {
+            localStorage.removeItem('checkIn');
+          }
+        })
+      } else {
+        alert('You must check in first.')
+      }
+    }
   }
 
   public navigateTo(url: string): void {
